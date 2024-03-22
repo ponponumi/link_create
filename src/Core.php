f232f22d63e15366611ec810189877a336a6d6a4
ponpon <ponponumi@gmail.com>
2024-03-23 01:33:12 +0900

    Web向けだったので記述場所を変更

--- src/Core.php
+++ src/Core.php
@@ -6,13 +6,6 @@ use Ponponumi\EmailSearch\EmailSearch;
 use Ponponumi\UrlSearch\UrlSearch;
 
 class Core{
-  public static function esc(string $text){
-    // 特殊文字、スペース、改行を置き換える
-    $text = htmlspecialchars($text,ENT_QUOTES,"UTF-8");
-    $text = str_replace([" ","\n"],["&nbsp;","<br>"],$text);
-    return $text;
-  }
-
   public static function get(string $text,array $type=["email","url"]){
     // データを取得する
     $email_list = [];
--- src/Web.php
+++ src/Web.php
@@ -3,5 +3,10 @@
 namespace Ponponumi\LinkCreate;
 
 class Web{
-  //
+  public static function esc(string $text){
+    // 特殊文字、スペース、改行を置き換える
+    $text = htmlspecialchars($text,ENT_QUOTES,"UTF-8");
+    $text = str_replace([" ","\n"],["&nbsp;","<br>"],$text);
+    return $text;
+  }
 }
