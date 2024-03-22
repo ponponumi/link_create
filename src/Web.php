22c511f099c3535b07b30f9830c697dfe4fb9b3d
ponpon <ponponumi@gmail.com>
2024-03-23 02:06:24 +0900

    aタグの生成機能を追加

--- src/Web.php
+++ src/Web.php
@@ -2,6 +2,9 @@
 
 namespace Ponponumi\LinkCreate;
 
+use Ponponumi\UrlSearch\UrlSearch;
+use Ponponumi\EmailSearch\EmailSearch;
+
 class Web{
   public static function esc(string $text){
     // 特殊文字、スペース、改行を置き換える
@@ -20,6 +23,12 @@ class Web{
       foreach ($list as $item) {
         if($item["link"] !== null){
           // リンクがあれば
+          if(UrlSearch::check($item["link"])){
+            // URLなら
+            $html .= '<a href="' . self:::esc($item["link"]) . '">' . self:::esc($item["text"]) . '</a>';
+          }elseif(EmailSearch::check($item["link"])){
+            $html .= '<a href="mailto:' . self:::esc($item["link"]) . '">' . self:::esc($item["text"]) . '</a>';
+          }
         }else{
           // リンクがなければ
           $html .= self:::esc($item["text"]);
