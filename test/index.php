d812a742f7de49eca75bbf7e74a2f50ab6e1babb
ponpon <ponponumi@gmail.com>
2024-03-23 01:29:04 +0900

    テストファイルを作成

--- test/index.php
+++ test/index.php
@@ -1,3 +1,16 @@
 <?php require __DIR__ . "/header.php" ?>
 
+<?php
+
+$text = "hello@example.com hoge@example.com http://example.com https://example.com";
+$get = \Ponponumi\LinkCreate\Core::get($text);
+$list = \Ponponumi\LinkCreate\Core::arrangement($text);
+
+?>
+
+<p><?= $text ?></p>
+
+<pre><?php var_dump($get) ?></pre>
+<pre><?php var_dump($list) ?></pre>
+
 <?php require __DIR__ . "/footer.php" ?>
