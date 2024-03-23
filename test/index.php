fd23ab59cfa7554b52718f9611fe5e37b596bf37
ponpon <ponponumi@gmail.com>
2024-03-23 14:33:23 +0900

    テストを修正

--- test/index.php
+++ test/index.php
@@ -2,10 +2,12 @@
 
 <?php
 
-$text = "hello@example.com hoge@example.com http://example.com https://example.com";
+$text = "hello@example.com hoge@example.com http://example.com https://example.com #PHP #こんにちは";
 $get = \Ponponumi\LinkCreate\Core::get($text);
 $list = \Ponponumi\LinkCreate\Core::arrangement($text);
-$a = \Ponponumi\LinkCreate\Web::create($text);
+$a = \Ponponumi\LinkCreate\Web::create($text,[
+  "hashtagUrl" => 'http://localhost/search.php?tag={hashtag}',
+]);
 
 ?>
 
