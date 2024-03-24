38e4b3163e0ebe7913688450d6496d6389c3f103
ponpon <ponponumi@gmail.com>
2024-03-25 00:16:59 +0900

    テストを追加

--- test/coretest.php
+++ test/coretest.php
@@ -10,6 +10,13 @@ $get_4 = \Ponponumi\LinkCreate\Core::get($text,["email","url"]);
 $get_5 = \Ponponumi\LinkCreate\Core::get($text,["url","hashtag"]);
 $get_6 = \Ponponumi\LinkCreate\Core::get($text,["email","hashtag"]);
 $get_7 = \Ponponumi\LinkCreate\Core::get($text);
+$arrangement_1 = \Ponponumi\LinkCreate\Core::arrangement($text,["email"]);
+$arrangement_2 = \Ponponumi\LinkCreate\Core::arrangement($text,["url"]);
+$arrangement_3 = \Ponponumi\LinkCreate\Core::arrangement($text,["hashtag"]);
+$arrangement_4 = \Ponponumi\LinkCreate\Core::arrangement($text,["email","url"]);
+$arrangement_5 = \Ponponumi\LinkCreate\Core::arrangement($text,["url","hashtag"]);
+$arrangement_6 = \Ponponumi\LinkCreate\Core::arrangement($text,["email","hashtag"]);
+$arrangement_7 = \Ponponumi\LinkCreate\Core::arrangement($text);
 
 ?>
 
@@ -22,5 +29,12 @@ $get_7 = \Ponponumi\LinkCreate\Core::get($text);
 <pre><?php var_dump($get_5) ?></pre>
 <pre><?php var_dump($get_6) ?></pre>
 <pre><?php var_dump($get_7) ?></pre>
+<pre><?php var_dump($arrangement_1) ?></pre>
+<pre><?php var_dump($arrangement_2) ?></pre>
+<pre><?php var_dump($arrangement_3) ?></pre>
+<pre><?php var_dump($arrangement_4) ?></pre>
+<pre><?php var_dump($arrangement_5) ?></pre>
+<pre><?php var_dump($arrangement_6) ?></pre>
+<pre><?php var_dump($arrangement_7) ?></pre>
 
 <?php require __DIR__ . "/footer.php" ?>
