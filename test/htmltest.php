5e07fd00d136752a5f768f736830bdebfe2b6848
ponpon <ponponumi@gmail.com>
2024-03-24 13:49:19 +0900

    ハッシュタグが変換されないことを確認

--- test/htmltest.php
+++ test/htmltest.php
@@ -9,11 +9,17 @@ $html_1 = \Ponponumi\LinkCreate\Web::create($text,[
 
 $html_2 = \Ponponumi\LinkCreate\Web::create($text,[]);
 
+$html_3 = \Ponponumi\LinkCreate\Web::create($text,[
+  "hashtagUrl" => 'http://localhost:2230/search.php?tag={hashtag}',
+  "hashtagNotGet" => true,
+]);
+
 ?>
 
 <p><?= $text ?></p>
 
 <p><?= $html_1 ?></p>
 <p><?= $html_2 ?></p>
+<p><?= $html_3 ?></p>
 
 <?php require __DIR__ . "/footer.php" ?>
