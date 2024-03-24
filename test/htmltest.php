ecc5f32e0d01ca92aad6414a4ec8701c546bb73c
ponpon <ponponumi@gmail.com>
2024-03-24 13:47:38 +0900

    オプション省略時、ハッシュタグがリンク化されないことを確認

--- test/htmltest.php
+++ test/htmltest.php
@@ -7,10 +7,13 @@ $html_1 = \Ponponumi\LinkCreate\Web::create($text,[
   "hashtagUrl" => 'http://localhost:2230/search.php?tag={hashtag}',
 ]);
 
+$html_2 = \Ponponumi\LinkCreate\Web::create($text,[]);
+
 ?>
 
 <p><?= $text ?></p>
 
 <p><?= $html_1 ?></p>
+<p><?= $html_2 ?></p>
 
 <?php require __DIR__ . "/footer.php" ?>
